a44f70bdda4379d4587b1d52e7ba6a6323e42e63
Santiago Mendez <fsmendez56@ucatolica.edu.co>
2025-01-29 17:52:10 -0500

    Add functions to add friends and clear input field

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriend() {
+  let friendName = document.getElementById('amigo').value;
+
+  if (friendName !== '') {
+    friends.push(friendName);
+    clearInputField();
+  } else {
+    alert('Por favor, inserte un nombre.');
+  }
+}
+
+function clearInputField() {
+  document.getElementById('amigo').value = '';
+}
