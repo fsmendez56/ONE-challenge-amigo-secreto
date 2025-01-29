f9b7b14903e1fc3d9db769e6701973667e0c3918
Santiago Mendez <fsmendez56@ucatolica.edu.co>
2025-01-29 18:26:07 -0500

    Add function to update friends list

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function addFriend() {
   if (friendName !== '') {
     friends.push(friendName);
     clearInputField();
+    updateFriendsList();
   } else {
     alert('Por favor, inserte un nombre.');
   }
@@ -13,4 +14,17 @@ function addFriend() {
 
 function clearInputField() {
   document.getElementById('amigo').value = '';
+}
+
+function updateFriendsList() {
+  let friendsList = document.getElementById('listaAmigos');
+  let items = '';
+
+  friendsList.innerHTML = '';
+
+  for (let i = 0; i < friends.length; i++) {
+    items += `<li>${friends[i]}</li>`;
+  }
+
+  friendsList.innerHTML = items;
 }
